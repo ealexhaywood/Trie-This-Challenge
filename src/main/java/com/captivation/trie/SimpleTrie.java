ec20ef00469de8a573d779f99aa086d154c9de8b
ealexhaywood <ealexhaywood@gmail.com>
2018-04-02 23:28:31 -0400

    Continue templating

---
+++ src/main/java/com/captivation/trie/SimpleTrie.java
@@ -0,0 +1,23 @@
+package com.captivation.trie;
+
+import java.util.List;
+
+/**
+ * Created by ealexhaywood on 4/2/18.
+ */
+public class SimpleTrie implements Trie {
+    @Override
+    public boolean add(String word) {
+        return false;
+    }
+
+    @Override
+    public boolean contains(String word) {
+        return false;
+    }
+
+    @Override
+    public List<String> search(String prefix) {
+        return null;
+    }
+}
--- src/main/java/com/captivation/trie/Trie.java
+++ src/main/java/com/captivation/trie/Trie.java
@@ -1,7 +1,17 @@
 package com.captivation.trie;
 
+import java.util.List;
+
 /**
  * Created by ealexhaywood on 4/2/18.
  */
-public class Trie {
+public interface Trie {
+    /*  adds word to the trie, returning true if the word was successfully added and false if the word was already present. */
+    boolean add(String word);
+
+    /*  returns true if the trie contains word and false if it does not. */
+    boolean contains(String word);
+
+    /*  returns the list of all words in the trie that begin with prefix. */
+    List<String> search(String prefix);
 }
