9b0af4cbf14a9cfaa5f8fe2599012ecbb94aeef4
ealexhaywood <ealexhaywood@gmail.com>
2018-04-04 17:21:56 -0400

    Add more test cases.  Add build dir to gitignore

--- src/test/java/com/captivation/trie/SimpleTrieTest.java
+++ src/test/java/com/captivation/trie/SimpleTrieTest.java
@@ -11,12 +11,21 @@ public class SimpleTrieTest {
     @Test
     public void addWordsTest() {
         Trie trie = new SimpleTrie();
-        trie.add("hello");
-        trie.add("this");
-        trie.add("is");
-        trie.add("a");
-        trie.add("test");
 
+        // Assert adding
+        Assert.assertTrue(trie.add("hello"));
+        Assert.assertFalse(trie.add("hello"));
+        Assert.assertTrue(trie.add("this"));
+        Assert.assertFalse(trie.add("this"));
+        Assert.assertTrue(trie.add("is"));
+        Assert.assertFalse(trie.add("is"));
+        Assert.assertTrue(trie.add("a"));
+        Assert.assertFalse(trie.add("a"));
+        Assert.assertTrue(trie.add("test"));
+        Assert.assertFalse(trie.add("test"));
+
+
+        // Assert contains
         Assert.assertTrue(trie.contains("hello"));
         Assert.assertTrue(trie.contains("this"));
         Assert.assertFalse(trie.contains("maybe"));
